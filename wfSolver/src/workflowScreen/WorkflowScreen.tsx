d06e75796d6fda625b61332c436d40a0347e2848
philip <philip.brandal@gmail.com>
2025-08-03 11:17:34 +0200

    Initial commit

---
+++ wfSolver/src/workflowScreen/WorkflowScreen.tsx
@@ -0,0 +1,13 @@
+import { Button } from "../components/Button"
+
+function WorkflowScreen () {
+
+    return (
+        <div>
+            <Button text="Klikk her">
+            </Button>
+        </div>
+    )
+}
+
+export default WorkflowScreen
